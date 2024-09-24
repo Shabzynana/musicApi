190e9940638cec1c4d7a1fdd1b08e7045131bf65
MAC <horlarmihleykan10@gmail.com>
2024-09-24 17:12:50 +0100

    feat: user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,41 @@
+
+import { IsEmail } from "class-validator";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne} from "typeorm";
+import { getIsInvalidMessage } from "../utils";
+import ExtendedBaseEntity from "../base-entity";
+@Entity({ name: "users" })
+export class User extends ExtendedBaseEntity {
+
+  @Column({ nullable: false })
+  first_name: string;
+
+  @Column({ nullable: false })
+  last_name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true, nullable: false })
+  @IsEmail(undefined, { message: getIsInvalidMessage("Email") })
+  email: string;
+
+  @Column()
+  password: string;
+  
+  @Column({ nullable: true })
+  google_id: string;
+
+  @Column({ default: false })
+  is_verified: boolean;
+
+  @Column({ nullable: true })
+  is_verified_date: Date;
+
+  @Column({ nullable: true })
+  image_url: string;
+
+  // Soft delete flag
+  @Column({ default: false })
+  is_deleted: boolean;
+
+}
