e31a1f5d721f5b7689b07fc4045f8c3bd1d5f375
MAC <horlarmihleykan10@gmail.com>
2024-09-24 10:57:22 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import cors from "cors";
+import express, { Express, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+
+
+// const CSS_URL =
+//   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+
+const app: Express = express();
+app.options("*", cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Authorization",
+    ],
+  }),
+);
+
+// app.use(Limiter);
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ limit: "15mb", extended: true }));
+// app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }));
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "I am the express API responding for Polling System",
+  });
+});
+
+
+
+// app.use("/openapi.json", (_req: Request, res: Response) => {
+//   res.setHeader("Content-Type", "application/json");
+//   res.send(swaggerSpec);
+// });
+
+
+export default app;
