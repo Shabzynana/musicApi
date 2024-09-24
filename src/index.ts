70033f37a40a22c03da4f08dc9b710b31e8d1512
MAC <horlarmihleykan10@gmail.com>
2024-09-24 13:13:50 +0100

    feat: data source

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
-// import AppDataSource from "./data-source";
+import AppDataSource from "./data-source";
 import app from "./app";
-// import config from "./config";
+import config from "./config";
 // import log from "./utils/logger";
 
 dotenv.config();
 
-// const port = config.port;
+const port = config.port;
 
 
-// AppDataSource.initialize()
-//   .then(async () => {
-//     app.listen(port, () => {
-//     //   log.info(`Server is listening on port ${port}`);
-//       console.log(`Server is listening on port ${port}`);
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(port, () => {
+    //   log.info(`Server is listening on port ${port}`);
+      console.log(`Server is listening on port ${port}`);
 
-//     });
-//   })
-// //   .catch((error) => log.error(error));
-//   .catch((error) => console.log(error));
+    });
+  })
+//   .catch((error) => log.error(error));
+  .catch((error) => console.log(error));
 
