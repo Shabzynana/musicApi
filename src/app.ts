5366892c6bc8a66a01e36fcd89a63d675bee74cd
MAC <horlarmihleykan10@gmail.com>
2024-09-27 16:51:17 +0100

    fix: swagger docs config

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    message: "I am the express API responding for Polling System",
+    message: "I am the express API responding for MusicApi",
   });
 });
 
